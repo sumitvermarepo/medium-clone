7bc8affd68c649ef89cb265cfbc0b80fa803c87b
amanjain97 <amanjain5221@gmail.com>
2022-06-05 02:03:22 +0530

    make it better

--- src/client/app/components/screens/BlogScreen/components/Footing/Footing.jsx
+++ src/client/app/components/screens/BlogScreen/components/Footing/Footing.jsx
@@ -6,9 +6,7 @@ const Footing = () => (
     <div className={styles.inner}>
       <nav>
         <ul>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://blog.kentcdodds.com/about">About kentcdodds</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://blog.kentcdodds.com/latest">Latest Stories</a></li>
-          <li><a target="_blank" rel="noopener noreferrer" href="https://blog.kentcdodds.com/archive">Archive</a></li>
+          <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/mauryasaurav">About me</a></li>
           <li><a target="_blank" rel="noopener noreferrer" href="https://about.medium.com/">About Medium</a></li>
           <li><a target="_blank" rel="noopener noreferrer" href="https://medium.com/policy/9db0094a1e0f">Terms</a></li>
           <li><a target="_blank" rel="noopener noreferrer" href="https://medium.com/policy/f03bf92035c9">Privacy</a></li>
